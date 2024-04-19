9601913fa5c2c1909273430a41eddf6eb1e1aae1
Keshore <keshore.muralidharan@emibg.com>
2024-04-19 21:49:50 +0530

    ec2 instance creation block update

--- maint.tf
+++ maint.tf
@@ -1,3 +1,24 @@
+resource "tls_private_key" "instance_key" {
+    algorithm = "RSA"
+}
+
+resource "aws_key_pair" "generate_key_pair" {
+    key_name = "instance_key"
+    public_key = tls_private_key.instance_key.public_key_openssh
+    depends_on = [
+        tls_private_key.instance_key
+    ]
+}
+
+resource "local_file" "generate_pem_file"{
+    content = tls_private_key.instance_key.private_key_pem
+    filename = "key-value.pem"
+    file_permission = "0400"
+    depends_on = [
+        tls_private_key.instance_key
+    ]
+}
+
 resource "aws_vpc" "vpc_for_aws_resource" {
     cidr_block = "${var.vpc_cidr_block}"
     enable_dns_support = true
@@ -76,17 +97,31 @@ resource "aws_dynamodb_table" "table_records" {
 
 resource "aws_vpc_endpoint" "connect_to_dynamodb"{
     vpc_id = aws_vpc.main.id
-    service_name = ""
-    vpc_endpoint_type = ""
+    service_name = "dynamodb.us-east-1.amazonaws.com"
+    vpc_endpoint_type = "Interface"
     private_dns_enabled = true
 }
 
-resource "aws_instance" "private_subnet" {
-    ami_id = ""
-    instance_type = ""
-    subnet_id = ""
-    vpc_security_ids = ""
+data "aws_ami" "ubuntu" {
+    most_recent = true
 
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
+    }
+}
+
+data "aws_subnet" "get_subnet_id" {
+  id = aws_subnet.configuring_vpc.id
+}
+
+resource "aws_instance" "launch_ec2_instance" {
+    ami_id = data.aws_ami.ubuntu.id
+    instance_type = "t2.micro"
+    subnet_id = data.aws_subnet.get_subnet_id
+    key_name = aws_key_pair.generate_key_pair.key_name
+    vpc_security_ids = [aws_security_group.sg_for_app.id]
+    associate_public_ip_address = true
     tags = {
         Project = "vpcendpoint-dynamodb-table"
     }
